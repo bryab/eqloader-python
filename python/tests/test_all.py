2ff7cd27bf053e5cc02c88267d628c1f9166774d
Bryan Fordney <126733+bryab@users.noreply.github.com>
2025-02-22 09:08:30 -0500

    quick test of reading filenames

--- python/tests/test_all.py
+++ python/tests/test_all.py
@@ -1,6 +1,13 @@
 import pytest
+from pathlib import Path
 import blender_eqloader
 
+fixtures_dir = Path(__file__).parent / 'fixtures'
 
-def test_sum_as_string():
-    assert blender_eqloader.sum_as_string(1, 1) == "2"
+def test_load_archive():
+    test_filename = fixtures_dir / 'rivervale.s3d'
+    archive = blender_eqloader.S3DArchive(str(test_filename))
+    #print(archive.name)
+    assert 'boathouse1.bmp' in archive.get_filenames()
+    print(len(archive.get_filenames()))
+    assert len(archive.get_filenames()) >= 87
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,37 @@
+use libeq_archive::EqArchive;
+use pyo3::exceptions::PyOSError;
 use pyo3::prelude::*;
+use std::ffi::OsStr;
+use std::fs::File;
+use std::path::Path;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[pyclass]
+struct S3DArchive {
+    archive: EqArchive,
+    /// The file stem of the archive, e.g. "rivervale".  This is used to get the main WLD out of the archive without specifying its name.
+    name: String,
+}
+
+#[pymethods]
+impl S3DArchive {
+    #[new]
+    fn new(filename: &str) -> PyResult<Self> {
+        let file = File::open(&filename)?;
+        let archive = EqArchive::read(file)
+            //.map_err(|e| Err(PyOSError::new_err("Fail")))
+            .unwrap();
+        let name = String::from(Path::new(&filename).file_stem().unwrap().to_str().unwrap());
+        Ok(S3DArchive { archive, name })
+    }
+
+    pub fn get_filenames(&mut self) -> PyResult<Vec<String>> {
+        Ok(self.archive.iter().map(|(s, _)| String::from(s)).collect())
+    }
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn blender_eqloader(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_class::<S3DArchive>()?;
     Ok(())
 }
